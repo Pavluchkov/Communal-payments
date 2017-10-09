f02050d17f4147f4aa20705fb20144f7931ad3eb
Anton Pavluchkov <pavluchkov1501@gmail.com>
2017-10-09 21:22:14 +0300

    В класс ObjectAccountController Добавлен метод textFilter устанавливающий фильтр на ввод текста в textField

--- src/by/javafx/communalPayments/controllers/objectAccounting/ObjAccountController.java
+++ src/by/javafx/communalPayments/controllers/objectAccounting/ObjAccountController.java
@@ -2,12 +2,17 @@ package by.javafx.communalPayments.controllers.objectAccounting;
 
 import by.javafx.communalPayments.controllers.Controller;
 import by.javafx.communalPayments.objects.ObjectAccounting;
+import javafx.collections.SetChangeListener;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
+import java.util.function.UnaryOperator;
 
 public class ObjAccountController extends Controller {
 
@@ -26,6 +31,39 @@ public class ObjAccountController extends Controller {
 
     @FXML
     private Button btnCancel;
+
+    private UnaryOperator<TextFormatter.Change> textFilter(String regexString) {
+        UnaryOperator<TextFormatter.Change> Filter = change -> {
+            String text = change.getText();
+
+            if (text.matches(regexString)) {
+                return change;
+            }
+
+            return null;
+        };
+
+        return Filter;
+    }
+
+    @FXML
+    private void initialize() {
+
+        TextFormatter<String> textFormatterPersonal = new TextFormatter<>(textFilter("[0-9]*"));
+        TextFormatter<String> textFormatterResident = new TextFormatter<>(textFilter("[0-9]*"));
+        TextFormatter<String> textFormatterArea = new TextFormatter<>(textFilter("[0-9.]*"));
+        personalAccount.setTextFormatter(textFormatterPersonal);
+        residents.setTextFormatter(textFormatterResident);
+        area.setTextFormatter(textFormatterArea);
+
+//        personalAccount.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+//            @Override public void handle(KeyEvent keyEvent) {
+//                if (!"0123456789".contains(keyEvent.getCharacter())) {
+//                    keyEvent.consume();
+//                }
+//            }
+//        });
+    }
 
     @FXML
     public void btnOkClicked() {
