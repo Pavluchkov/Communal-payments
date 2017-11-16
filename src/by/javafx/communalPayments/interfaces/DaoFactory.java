851344c7ed2a516cfe5d2942159c6bcd78014c50
Pavluchkov A.V <pavluchkov1501@gmail.com>
2017-11-16 21:06:03 +0300

    Доделан интерфейс DaoFactory

--- src/by/javafx/communalPayments/interfaces/DaoFactory.java
+++ src/by/javafx/communalPayments/interfaces/DaoFactory.java
@@ -5,6 +5,10 @@ import java.sql.SQLException;
 
 public interface DaoFactory {
     Connection getConnection() throws SQLException;
-    ObjectAccountDao getObjectAccountDao();
-
+    ObjectAccountDao getObjectAccountDao(Connection connection);
+    CountersDao getCountersDao(Connection connection);
+    ServicesDao getServicesDao(Connection connection);
+    PaymentsDao getPaymentsDao(Connection connection);
+    MeasurementDao getMeasurementDao(Connection connection);
+    FormPaymentsDao getFormPaymentsDao(Connection connection);
 }
